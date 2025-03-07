218cae28a8980371ffbc49240c645ca2f72c57e5
wobsoriano <sorianorobertc@gmail.com>
2025-03-07 10:30:44 -0800

    revert

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from clerk_backend_api import Clerk
 from clerk_backend_api.jwks_helpers import AuthenticateRequestOptions
+import httpx
 
 load_dotenv()
 
@@ -17,10 +18,21 @@ clerk_secret_key=os.getenv('CLERK_SECRET_KEY')
 
 sdk = Clerk(bearer_auth=clerk_secret_key)
 
+def fastapi_to_httpx_request(request: Request) -> httpx.Request:
+    return httpx.Request(
+        method=request.method,
+        url=str(request.url),
+        headers=request.headers,
+    )
+
 @app.middleware('http')
 async def authenticate_with_clerk(request: Request, call_next):
-    options = AuthenticateRequestOptions()
-    request_state = sdk.authenticate_request(request, options)
+    options = AuthenticateRequestOptions(
+        secret_key=clerk_secret_key
+    )
+    
+    httpx_request = fastapi_to_httpx_request(request)
+    request_state = sdk.authenticate_request(httpx_request, options)
     request.state.user_id = None if request_state.payload is None else request_state.payload['sub']
     response = await call_next(request)
     return response
